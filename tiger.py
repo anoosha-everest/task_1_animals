1a391fbe97c1aba693e94ed64425f751cdbca6eb
Anoosha <anoosha.sanugula@everest.engineering>
2024-02-24 15:22:09 +0530

    Tiger is created

---
+++ tiger.py
@@ -0,0 +1,9 @@
+class Tiger:
+     def eat(self):
+        print("I can Eat all types of food")
+
+     def sleep(self):
+        print("I can Sleep by making snoring sound")
+
+     def sound(self):
+        print("I can Sound like roaring")
